65d8e2209fc7d116ed4dcbdbba559278ca28b12d
Marek Bartik <mab@revolgy.com>
2018-10-09 13:57:16 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+
+
+aws iam put-role-policy --role-name EKS-mab-eks-test-DefaultNodeGroup-NodeInstanceRole-E5NJLRIJDGSY  --policy-name mab-test-alb-ingress-extra --policy-document file://iam-policy.json
